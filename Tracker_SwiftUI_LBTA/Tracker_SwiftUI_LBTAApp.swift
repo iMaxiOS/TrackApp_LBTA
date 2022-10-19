b631761d8bd754339e4062c8c94ac4e2f6de3144
Maxim <ukr-maksim1@yandex.ru>
2022-10-19 13:51:25 +0300

    start project

---
+++ Tracker_SwiftUI_LBTA/Tracker_SwiftUI_LBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_SwiftUI_LBTAApp.swift
+//  Tracker_SwiftUI_LBTA
+//
+//  Created by Maxim Hranchenko on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_SwiftUI_LBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
